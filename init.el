9c2da0f06fcc09051684d9c4434d39eb91af855d
Eamon Taaffe <eamontaaffe@gmail.com>
2018-07-16 22:35:59 +1000

    Backups

--- init.el
+++ init.el
@@ -69,6 +69,24 @@ point reaches the beginning or end of the buffer, stop there."
 		'smarter-move-beginning-of-line)
 
 ;;;; End sane defaults ;;;;
+;;;; Backups ;;;;
+
+(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
+(setq delete-old-versions -1)
+(setq version-control t)
+(setq vc-make-backup-files t)
+(setq auto-save-file-name-transforms '((".*" "~/.emacs.d/auto-save-list/" t)))
+(setq savehist-file "~/.emacs.d/savehist")
+(savehist-mode 1)
+(setq history-length t)
+(setq history-delete-duplicates t)
+(setq savehist-save-minibuffer-history 1)
+(setq savehist-additional-variables
+      '(kill-ring
+        search-ring
+        regexp-search-ring))
+
+;;;; Package stuff ;;;;
 
 (use-package helm
   :config
