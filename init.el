1bcea16ffa701e2110a2deae94f05969b6af9147
Eamon Taaffe <eamontaaffe@gmail.com>
2018-07-16 22:36:37 +1000

    Update installed packages

--- init.el
+++ init.el
@@ -141,7 +141,7 @@ point reaches the beginning or end of the buffer, stop there."
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (multiple-cursors magit helm-projectile helm projectile))))
+    (reason-mode multiple-cursors magit helm-projectile helm projectile))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
