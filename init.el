c6b5cfc4340bfae215f6cacc3bc9af1e0314cc50
Eamon Taaffe <eamontaaffe@gmail.com>
2018-07-16 21:52:44 +1000

    Initial commit

---
+++ init.el
@@ -0,0 +1,128 @@
+(require 'package)
+
+(add-to-list 'package-archives
+             '("melpa" . "https://stable.melpa.org/packages/") t)
+
+(package-initialize)
+
+;; No splash screen
+(setq inhibit-startup-message t)
+
+;; Window
+(menu-bar-mode -1)
+(toggle-scroll-bar -1)
+(tool-bar-mode -1)
+
+;; Set up package management with `use-package`
+
+(eval-when-compile
+  (add-to-list 'load-path "./use-package/use-package.el")
+  (require 'use-package))
+
+;;;; Sane defaults ;;;;
+
+;; Allow pasting from system clipboard
+(setq x-select-enable-clipboard t)
+
+;; Show keystrokes in progress
+(setq echo-keystrokes 0.1)
+
+;; Show keystrokes in progress
+(setq echo-keystrokes 0.1)
+
+;; Answering just 'y' or 'n' will do
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; Lines should be 80 characters wide, not 72
+(setq fill-column 80)
+
+;; 80 chars is a good width.
+(set-default 'fill-column 80)
+
+;; Spaces instead of tabs
+(setq-default indent-tabs-mode nil)
+
+;; Beginning of line
+(defun smarter-move-beginning-of-line (arg)
+  "Move point back to indentation of beginning of line.
+Move point to the first non-whitespace character on this line.
+If point is already there, move to the beginning of the line.
+Effectively toggle between the first non-whitespace character and
+the beginning of the line.
+If ARG is not nil or 1, move forward ARG - 1 lines first.  If
+point reaches the beginning or end of the buffer, stop there."
+  (interactive "^p")
+  (setq arg (or arg 1))
+
+  ;; Move lines first
+  (when (/= arg 1)
+    (let ((line-move-visual nil))
+      (forward-line (1- arg))))
+
+  (let ((orig-point (point)))
+    (back-to-indentation)
+    (when (= orig-point (point))
+      (move-beginning-of-line 1))))
+
+;; remap C-a to `smarter-move-beginning-of-line'
+(global-set-key [remap move-beginning-of-line]
+		'smarter-move-beginning-of-line)
+
+;;;; End sane defaults ;;;;
+
+(use-package helm
+  :config
+  (helm-mode 1)
+  (require 'helm-config)
+  :bind (("M-x" . helm-M-x)
+         ("C-x C-f" . helm-find-files))
+  :ensure t)
+
+(use-package helm-ls-git
+  :bind (("C-c g" . helm-ls-git-ls)
+         ("C-x C-d" . helm-browse-project)
+         ("M-y" . helm-show-kill-ring))
+  :ensure t)
+
+(use-package helm-projectile
+  :requires helm
+  :ensure t)
+
+(use-package projectile
+  :requires helm-projectile
+  :ensure t
+  :init
+  (setq projectile-enable-caching t)
+  (setq projectile-completion-system 'helm)
+  :config
+  (projectile-global-mode t)
+  (helm-projectile-on)
+  (global-set-key (kbd "C-c p .") 'helm-projectile-find-file-dwim))
+
+(use-package magit
+  :ensure t
+  :config
+  (global-set-key (kbd "C-x g") 'magit-status))
+
+(use-package multiple-cursors
+  :bind (("C-S-c C-S-c" . mc/edit-lines)
+         ("C->" . mc/mark-next-like-this)
+         ("C-<" . mc/mark-previous-like-this)
+         ("C-c C-<" . mc/mark-all-like-this))
+  :ensure t)
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (multiple-cursors magit helm-projectile helm projectile))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
