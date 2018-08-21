69c47e45387a21ecb6ca45e7f8aca33f0061782d
Eamon Taaffe <eamontaaffe@gmail.com>
2018-08-21 19:10:46 +1000

    Graphql mode

--- init.el
+++ init.el
@@ -194,6 +194,11 @@ point reaches the beginning or end of the buffer, stop there."
 
 (setq org-duration-format (quote h:mm))
 
+;; Graphql
+
+(use-package graphql-mode
+  :ensure t)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
