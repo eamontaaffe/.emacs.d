70660295620cfd405ffc86376c0a51ae53a1eebf
Eamon Taaffe <eamon.taaffe@Eamons-MacBook-Pro.local>
2019-01-21 17:17:22 +1100

    Remove old stuff that isn't necessary anymore

--- init.el
+++ init.el
@@ -20,8 +20,9 @@
 (menu-bar-mode -1)
 (toggle-scroll-bar -1)
 (tool-bar-mode -1)
-(add-to-list 'default-frame-alist '(height . 300))
+(add-to-list 'default-frame-alist '(height . 80))
 (add-to-list 'default-frame-alist '(width . 80))
+(set-face-attribute 'default nil :height 120)
 
 ;;;; Sane defaults ;;;;
 
@@ -101,6 +102,7 @@ point reaches the beginning or end of the buffer, stop there."
   :config
   (helm-mode 1)
   (require 'helm-config)
+
   :bind (("M-x" . helm-M-x)
          ("C-x C-f" . helm-find-files)
          ("C-c h" . helm-mini)
@@ -145,25 +147,7 @@ point reaches the beginning or end of the buffer, stop there."
          ("C-c C-<" . mc/mark-all-like-this))
   :ensure t)
 
-(use-package reason-mode
-  :ensure t)
-
 ;;; Other stuff ;;;
-
-(add-hook 'json-mode-hook
-          (lambda ()
-            (make-local-variable 'js-indent-level)
-            (setq js-indent-level 2)))
-(use-package yaml-mode
-  :config
-  (add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
-  :ensure t)
-
-(use-package rjsx-mode
-  :config
-  (add-to-list 'auto-mode-alist '("\\.js\\'" . rjsx-mode))
-  (setq js2-basic-offset 2)
-  :ensure t)
 
 (use-package doom-themes
   :config
@@ -175,53 +159,9 @@ point reaches the beginning or end of the buffer, stop there."
   (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
   :ensure t)
 
-(use-package toc-org
-  :config
-  (add-hook 'org-mode-hook 'toc-org-enable)
-  :ensure t)
-
 (use-package undo-tree
   :config
   (global-undo-tree-mode)
-  :ensure t)
-
-;; Typescript
-
-(use-package typescript-mode
-  :ensure t)
-
-(use-package tide
-  :ensure t)
-
-;; Terraform
-
-(use-package terraform-mode
-  :ensure t)
-
-(defun setup-tide-mode ()
-  (interactive)
-  (tide-setup)
-  (flycheck-mode +1)
-  (setq flycheck-check-syntax-automatically '(save mode-enabled))
-  (eldoc-mode +1)
-  (tide-hl-identifier-mode +1)
-  (setq indent-tabs-mode t)
-  (setq tab-width 4)
-  (setq typescript-indent-offset 4))
-
-;; aligns annotation to the right hand side
-(setq company-tooltip-align-annotations t)
-
-;; formats the buffer before saving
-(add-hook 'before-save-hook 'tide-format-before-save)
-
-(add-hook 'typescript-mode-hook #'setup-tide-mode)
-
-;; Haskell
-
-(use-package intero
-  :config
-  (intero-global-mode 1)
   :ensure t)
 
 ;; Org mode
@@ -232,171 +172,49 @@ point reaches the beginning or end of the buffer, stop there."
 
 (setq org-agenda-files '("~/org"))
 
-;; (use-package ox-pandoc
-;;   :ensure t)
-
 (add-hook 'org-mode-hook 'visual-line-mode)
 
 (setq org-duration-format (quote h:mm))
-
-;;; Org capture setup
-
-(setq org-default-notes-file (concat org-directory "/capture.org"))
-
-(define-key global-map "\C-cc" 'org-capture)
-
-(setq org-capture-templates
-      '(("c" "Cadmus task" entry
-         (file+headline "~/org/cadmus.org" "Tasks")
-         "** TODO [#B] %? :cadmus:\n %i" :prepend t)
-        
-        ("h" "Home task" entry (file+headline "~/org/home.org" "Tasks")
-         "** TASK %? :home:\n %i" :prepend t)))
-
-(define-key global-map "\C-cj" 'org-clock-jump-to-current-clock)
-
-;; Graphql
-
-(use-package graphql-mode
-  :ensure t)
 
 ;; Markdown
 
 (use-package markdown-mode
   :ensure t)
 
-;; Idris
-
-(use-package idris-mode
-  :ensure t)
-
-;; Elm
-
-(use-package elm-mode
-  :ensure t)
-
-;; Column enforcer
-
-(use-package column-enforce-mode
-  :config
-  (80-column-rule)
-  :ensure t)
-
-;; Docker mode
-
-(use-package dockerfile-mode
-  :ensure t)
-
-;; Latex mode
-
-(use-package exec-path-from-shell
-  :config
-  (when (memq window-system '(mac ns x)) (exec-path-from-shell-initialize))
-  :ensure t)
-
-(use-package auctex
-  :requires exec-path-from-shell
-  :defer t
-  :init
-  (setq-default TeX-engine 'xetex)
-  (setq-default TeX-PDF-mode t)
-  :ensure t)
-
-;; Dash documentation
-
-(use-package helm-dash
-  :bind (("C-c d" . helm-dash))
-  :config
-  (setq helm-dash-docsets-path "~/.docsets/")
-  (setq browse-url-browser-function 'browse-url-generic
-      browse-url-generic-program "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome")
-  (setq helm-dash-browser-func 'browse-url-generic)
-  (setq helm-dash-common-docsets '("Redis" "Haskell"))
-  :ensure t)
-
-(defun haskell-doc ()
-  (interactive)
-  (setq-local helm-dash-docsets '("Haskell")))
-
-(add-hook 'haskell-mode-hook 'haskell-doc)
-
-;; Engine mode
-
-(use-package engine-mode
-  :config
-  (defengine hoogle
-  "https://www.haskell.org/hoogle/?hoogle=%s"
-  :keybinding "h"
-  :docstring "Searchin' for Haskell types or identifiers."))
-
 ;; Custom functions
 
-(defun frame-default ()
+
+(defun frame-single ()
   (interactive)
   (if (window-system)
-      (set-frame-size (selected-frame) 80 100)))
+      (set-frame-size (selected-frame) 80 80)))
 
 (defun frame-double ()
   (interactive)
   (if (window-system)
-      (set-frame-size (selected-frame) 163 100)))
+      (set-frame-size (selected-frame) 163 80)))
 
-;; Dhall
+(defun font-large ()
+  (interactive)
+  (set-face-attribute 'default nil :height 150))
 
-(use-package dhall-mode
-  :init
-  (setq dhall-format-command nil)
-  (setq dhall-at-save nil)
-  (setq dhall-use-header-line nil)
-  :mode "\\.dhall\\'")
+(defun font-medium ()
+  (interactive)
+  (set-face-attribute 'default nil :height 120))
 
-;; Racket
+;; Generated stuff, don't edit
 
-(use-package racket-mode)
-
-;; Generated stuff (don't edit)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#191C25" "#C16069" "#A2BF8A" "#ECCC87" "#80A0C2" "#B58DAE" "#86C0D1" "#F0F4FC"])
- '(custom-safe-themes
-   (quote
-    ("f0dc4ddca147f3c7b1c7397141b888562a48d9888f1595d69572db73be99a024" "6d589ac0e52375d311afaa745205abb6ccb3b21f6ba037104d71111e7e76a3fc" "fe666e5ac37c2dfcf80074e88b9252c71a22b6f5d2f566df9a7aa4f9bea55ef8" "3a3de615f80a0e8706208f0a71bbcc7cc3816988f971b6d237223b6731f91605" "6b2636879127bf6124ce541b1b2824800afc49c6ccd65439d6eb987dbf200c36" "d2e9c7e31e574bf38f4b0fb927aaff20c1e5f92f72001102758005e53d77b8c9" "8dce5b23232d0a490f16d62112d3abff6babeef86ae3853241a85856f9b0a6e7" "d97baf5a34c87b05508739505cad03438cde8efa2a0d350c7773f2a8bc26a50d" "099c44618d7660548701d4f495a8c23a85103bc7b87fec33c9db4cd099a4adaf" default)))
- '(fci-rule-color "#4C566A")
- '(jdee-db-active-breakpoint-face-colors (cons "#10151C" "#5EC4FF"))
- '(jdee-db-requested-breakpoint-face-colors (cons "#10151C" "#8BD49C"))
- '(jdee-db-spec-breakpoint-face-colors (cons "#10151C" "#384551"))
- '(markdown-command "/usr/local/bin/pandoc")
- '(org-tags-column -65)
  '(package-selected-packages
    (quote
-    (neotree fsharp-mode elixir-mode synonymous cider racket-mode dhall-mode engine-mode zeal-at-point helm-dash doom-themes json-mode auctex exec-path-from-shell terraform-mode tide typescript-mode dockerfile-mode rjsx-mode column-enforce-mode intero interleave elm-mode idris-mode ox-pandoc ox-md markdown-mode undo-tree graphql-mode toc-org rainbow-delimiters web-mode exec-path helm-ag basic-theme white-theme twilight-bright-theme reason-mode multiple-cursors magit helm-projectile helm projectile)))
- '(safe-local-variable-values
-   (quote
-    ((intero-targets "resources:lib" "resources:exe:resources-exe" "resources:test:resources-test"))))
- '(vc-annotate-background "#ffffff")
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#ab4642")
-     (50 . "#dc9656")
-     (80 . "#f7ca88")
-     (110 . "#a1b56c")
-     (140 . "#86c1b9")
-     (170 . "#7cafc2")
-     (200 . "#ab4642")
-     (230 . "#a16046")
-     (260 . "#181818")
-     (290 . "#282828")
-     (320 . "#383838")
-     (350 . "#585858"))))
- '(vc-annotate-very-old-color "#585858"))
+    (markdown-mode undo-tree rainbow-delimiters doom-themes multiple-cursors magit helm-ag helm-projectile helm-ls-git helm use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-(put 'narrow-to-region 'disabled nil)
