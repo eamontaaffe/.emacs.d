a16b113a76f9c683bdb131adfafcc75b63710db7
Eamon Taaffe <eamontaaffe@gmail.com>
2019-02-18 22:19:45 +1100

    Updated `custom.el`

--- custom.el
+++ custom.el
@@ -3,9 +3,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
+ '(ansi-color-names-vector
+   ["#FFFFFF" "#d15120" "#5f9411" "#d2ad00" "#6b82a7" "#a66bab" "#6b82a7" "#505050"])
+ '(ansi-term-color-vector
+   [unspecified "#FFFFFF" "#d15120" "#5f9411" "#d2ad00" "#6b82a7" "#a66bab" "#6b82a7" "#505050"])
+ '(custom-safe-themes
    (quote
-    (twilight-bright-theme ag exec-path-from-shell diff-hl dhall-mode wsd-mode elm-mode scala-mode alchemist elixir-mode yaml-mode use-package undo-tree rainbow-delimiters multiple-cursors markdown-mode magit helm-projectile helm-ls-git helm-ag doom-themes))))
+    ("100e7c5956d7bb3fd0eebff57fde6de8f3b9fafa056a2519f169f85199cc1c96" default)))
+ '(fci-rule-character-color "#d9d9d9")
+ '(fci-rule-color "#d9d9d9"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
