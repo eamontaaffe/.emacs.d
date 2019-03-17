6231f42fa1bfe5b460c2f71dc5f1747dc9af7f19
Eamon Taaffe <eamontaaffe@gmail.com>
2019-03-18 09:14:24 +1100

    Update custom variables

--- custom.el
+++ custom.el
@@ -3,15 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#FFFFFF" "#d15120" "#5f9411" "#d2ad00" "#6b82a7" "#a66bab" "#6b82a7" "#505050"])
- '(ansi-term-color-vector
-   [unspecified "#FFFFFF" "#d15120" "#5f9411" "#d2ad00" "#6b82a7" "#a66bab" "#6b82a7" "#505050"])
- '(custom-safe-themes
+ '(package-selected-packages
    (quote
-    ("100e7c5956d7bb3fd0eebff57fde6de8f3b9fafa056a2519f169f85199cc1c96" default)))
- '(fci-rule-character-color "#d9d9d9")
- '(fci-rule-color "#d9d9d9"))
+    (diff-hl alchemist yaml-mode use-package undo-tree twilight-bright-theme theme-changer rainbow-delimiters neotree multiple-cursors markdown-mode magit json-snatcher json-reformat helm-projectile helm-ls-git helm-ag groovy-mode exec-path-from-shell ensime doom-themes dockerfile-mode ag))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
