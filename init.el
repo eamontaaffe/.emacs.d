b889736eb1c4a68c093bcc816a670364eb1be3b6
Eamon Taaffe <eamontaaffe@gmail.com>
2019-07-13 23:24:54 +1000

    Install crux to replace smarter-move-beginnning-of-line

--- init.el
+++ init.el
@@ -76,33 +76,14 @@
 
 (delete-selection-mode 1)
 
-;; Beginning of line
+;; Crux (mostly for to replace smarter beginning of line)
 
-(defun smarter-move-beginning-of-line (arg)
-  "Move point back to indentation of beginning of line.
-Move point to the first non-whitespace character on this line.
-If point is already there, move to the beginning of the line.
-Effectively toggle between the first non-whitespace character and
-the beginning of the line.
-If ARG is not nil or 1, move forward ARG - 1 lines first.  If
-point reaches the beginning or end of the buffer, stop there."
-  (interactive "^p")
-  (setq arg (or arg 1))
-
-  ;; Move lines first
-  (when (/= arg 1)
-    (let ((line-move-visual nil))
-      (forward-line (1- arg))))
-
-  (let ((orig-point (point)))
-    (back-to-indentation)
-    (when (= orig-point (point))
-      (move-beginning-of-line 1))))
-
-;; Remap C-a to `smarter-move-beginning-of-line'
-
-(global-set-key [remap move-beginning-of-line]
-		'smarter-move-beginning-of-line)
+(use-package crux
+  :bind
+  (("C-a" . crux-move-beginning-of-line)
+   ("C-c k" . crux-kill-other-buffers)
+   ("C-c I" . crux-find-user-init-file))
+  :ensure t)
 
 ;; UTF-8
 
