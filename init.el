3d98d7b932d7df3df5237311aabc87cb74618bf3
Eamon Taaffe <7099155+eamontaaffe@users.noreply.github.com>
2020-07-11 14:38:19 +1000

    Toggle scroll bar when in graphic mode only

--- init.el
+++ init.el
@@ -6,11 +6,11 @@
 
 (defvar default-window-height 40)
 
-(menu-bar-mode -1)
-
-(toggle-scroll-bar -1)
-
-(tool-bar-mode -1)
+(if (display-graphic-p)
+    (progn
+      (menu-bar-mode -1)
+      (toggle-scroll-bar -1)
+      (tool-bar-mode -1)))
 
 ;; Package
 
