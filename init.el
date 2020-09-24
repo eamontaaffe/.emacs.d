20c04a317126057cb88bd4be4b67c7de61d8a725
Eamon Taaffe <eamon@taaffe.com.au>
2020-09-24 14:31:56 +1000

    Add ts files to web-mode hook

--- init.el
+++ init.el
@@ -340,7 +340,7 @@
   (setq web-mode-code-indent-offset 2)
   (setq web-mode-css-indent-offset 2)
   :config
-  (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.tsx?\\'" . web-mode))
   (add-hook 'web-mode-hook
           (lambda ()
             (when (string-equal "tsx" (file-name-extension buffer-file-name))
